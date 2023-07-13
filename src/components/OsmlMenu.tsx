9793a53cf681816ace1b1e8ad7a3f99eae71d84f
William Duhe <drduhe@amazon.com>
2023-07-13 14:53:29 -0600

    Initial commit setting up solutions library repository

---
+++ src/components/OsmlMenu.tsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import {ButtonDropdown} from "@cloudscape-design/components";
+import Icon from "@cloudscape-design/components/icon";
+import LoadDataModal from "./modal/LoadDataModal";
+import ImageRequestModal from "@/components/modal/ImageRequestModal";
+import CredsExpiredAlert from "./alert/CredsExpiredAlert";
+
+
+const OsmlMenu = ({
+                      imageRequestStatus,
+                      setImageRequestStatus
+                  }: { imageRequestStatus: any, setImageRequestStatus: any }) => {
+    // Set initial state
+    const [showImageRequestModal, setShowImageRequestModal] = useState(false);
+    const [showLoadDataModal, setShowLoadDataModal] = useState(false);
+    const [showCredsExpiredAlert, setShowCredsExpiredAlert] = useState(false);
+    return (
+        <div>
+            <div style={{position: "absolute", top: 10, left: 10}}>
+                <ButtonDropdown
+                    items={[
+                        {text: "Submit Image Request", id: "new_request"},
+                        {
+                            id: "load",
+                            text: "Load",
+                            items: [
+                                { id: "load_geojson", text: "GeoJSON" },
+                                { id: "load_image", text: "Image"}
+                            ]
+                        }
+                    ]}
+                    onItemClick={(e) => {
+                        if (e.detail.id == "load_geojson") {
+                            setShowLoadDataModal(true)
+                        } else if (e.detail.id == "new_request") {
+                            setShowImageRequestModal(true)
+                        }
+                    }}
+                    variant="primary"
+                >
+                    <Icon name="menu"/>
+                </ButtonDropdown>
+            </div>
+            <div style={{position: "absolute", top: 10, right: 10}}>
+                {showCredsExpiredAlert &&
+                    <CredsExpiredAlert setShowCredsExpiredAlert={setShowCredsExpiredAlert}/>}
+            </div>
+
+            <LoadDataModal showLoadDataModal={showLoadDataModal}
+                           setShowLoadDataModal={setShowLoadDataModal}
+                           showCredsExpiredAlert={showCredsExpiredAlert}
+                           setShowCredsExpiredAlert={setShowCredsExpiredAlert}/>
+            <ImageRequestModal showImageRequestModal={showImageRequestModal}
+                               setShowImageRequestModal={setShowImageRequestModal}
+                               imageRequestStatus={imageRequestStatus}
+                               setImageRequestStatus={setImageRequestStatus}
+                               showCredsExpiredAlert={showCredsExpiredAlert}
+                               setShowCredsExpiredAlert={setShowCredsExpiredAlert}/>
+        </div>
+    );
+};
+
+export default OsmlMenu;
---
+++ src/components/alert/CredsExpiredAlert.tsx
@@ -0,0 +1,47 @@
+import { Button, Alert} from "@cloudscape-design/components";
+import { useState } from "react";
+import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
+import { getAWSCreds, REGION } from "@/config";
+
+const startingAlertMessage = "Refresh your credentials before closing this alert."
+
+const CredsExpiredAlert = ({
+                           setShowCredsExpiredAlert
+                       }: { setShowCredsExpiredAlert: any }) => {
+    const [alertMessage, setAlertMessage] = useState(startingAlertMessage);
+
+    const updateAlert = async () => {
+        try {
+            // check if the creds are still expired by making an API call
+            const response = await new STSClient({region: REGION, credentials: getAWSCreds()}).send(
+                new GetCallerIdentityCommand({}),
+            );
+            // if the code reached this point then the token refreshed successfully
+            setShowCredsExpiredAlert(false)
+            // Reset the alert message for next time
+            setAlertMessage(startingAlertMessage)
+        } catch (e: any) {
+            console.error(`Exception caught: ${e}`)
+            if (e.name === 'ExpiredToken') {
+                setAlertMessage("AWS token still is expired. Refresh credentials and try again.")
+            } else {
+                // Something else went wrong
+                setAlertMessage("Uknown error occurred when testing credentials. Restart application.")
+            }
+        }
+    };
+    return (
+        
+        <Alert
+            statusIconAriaLabel="Error"
+            type="error"
+            header="Credentials have expired."
+            dismissible
+            onDismiss={() => updateAlert()}
+        >
+            {alertMessage}
+        </Alert>
+    );
+};
+
+export default CredsExpiredAlert;
