053fbeafbaa8924b6be076244b3450c52d97a06e
Ranbir Aulakh <ranbiraulakhme@gmail.com>
2023-11-07 17:57:14 -0500

    feat: Add Github workflows

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function generateImageryProviders() {
     iconUrl: "https://doc.arcgis.com/en/data-appliance/2022/maps/GUID-BBDE7FFC-3B4D-4CCC-8117-20F8102CA192-web.jpg",
     tooltip: "This map is a compilation of satellite and aerial imagery worldwide. " +
         "https://doc.arcgis.com/en/data-appliance/2022/maps/world-imagery.htm",
+    // @ts-ignore */
     creationFunction: function () {
       return Cesium.ArcGisMapServerImageryProvider.fromUrl(
           "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer"
@@ -25,6 +26,7 @@ function generateImageryProviders() {
     iconUrl: "https://doc.arcgis.com/en/data-appliance/2022/maps/GUID-D521640A-4B9F-4588-BB9B-D8CCF5B950C4-web.jpg",
     tooltip: "This map presents a street map with highway-level data for the world and detailed streets in many " +
         "populated areas of the world. https://doc.arcgis.com/en/data-appliance/2022/maps/world-street-map.htm",
+    // @ts-ignore */
     creationFunction: function () {
       return Cesium.ArcGisMapServerImageryProvider.fromUrl(
           "https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"
@@ -36,6 +38,7 @@ function generateImageryProviders() {
     iconUrl: "https://doc.arcgis.com/en/data-appliance/2022/maps/GUID-AC8A1022-C5A4-45A0-9BF8-61E5C5C72C3F-web.jpg",
     tooltip: "This basemap provides a neutral background for your data with minimal colors, labels, and " +
         "features. https://doc.arcgis.com/en/data-appliance/2022/maps/world-light-gray-base.htm",
+    // @ts-ignore */
     creationFunction: function () {
       return Cesium.ArcGisMapServerImageryProvider.fromUrl(
           "https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer"
@@ -47,6 +50,7 @@ function generateImageryProviders() {
     iconUrl: "https://doc.arcgis.com/en/data-appliance/2022/maps/GUID-9F6C8013-DCC6-4D20-8E5A-E8C7F8F262E9-web.jpg",
     tooltip: "This map draws attention to your thematic content by providing a neutral background with minimal " +
         "colors, labels, and features. https://doc.arcgis.com/en/data-appliance/2022/maps/world-dark-gray-base.htm",
+    // @ts-ignore */
     creationFunction: function () {
       return Cesium.ArcGisMapServerImageryProvider.fromUrl(
           "https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer"
